d13dc949a8bbfe9a8e2002d128bf57278fd877af
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2025-03-04 12:32:08 +0000

    Minor p5 background

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,20 @@ import "./App.css";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./landingPage";
 import Details from "./mainPage";
+import { ReactP5Wrapper } from "@p5-wrapper/react";
+import { sketch } from "./sketch";
 
 function App() {
   return (
-    <Router>
-      <Routes key={location.pathname}>
-        <Route path="/" exact element={<LandingPage />} />
-        <Route path="/works" element={<Details />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes key={location.pathname}>
+          <Route path="/" exact element={<LandingPage />} />
+          <Route path="/works" element={<Details />} />
+        </Routes>
+      </Router>
+      <ReactP5Wrapper sketch={sketch} />
+    </>
   );
 }
 
