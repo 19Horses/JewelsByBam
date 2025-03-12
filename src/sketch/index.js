3003cefa70a52c1addeaef9d582b94177b83b1f5
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2025-03-12 15:39:39 +0000

    Reduce size again

--- src/sketch/index.js
+++ src/sketch/index.js
@@ -15,7 +15,7 @@ export function sketch(p5) {
     p5.background(255);
     pts.forEach(({ x, y }) => {
       const d = p5.dist(p5.mouseX, p5.mouseY, x, y);
-      const sz = p5.map(d, 0, 400, 5, 2, true);
+      const sz = p5.map(d, 0, 400, 3, 1.25, true);
       const alpha = p5.map(d, 0, 400, 200, 150, true);
       p5.fill(250, 128, 114, alpha);
       p5.circle(x, y, sz);
