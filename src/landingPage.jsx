990356333eb9e1c801034952a5fe8c78eeb7dcd2
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2025-03-12 14:07:17 +0000

    Move camera in and out on toggle

--- src/landingPage.jsx
+++ src/landingPage.jsx
@@ -1,33 +1,50 @@
 import { OrbitControls, useGLTF, useProgress } from "@react-three/drei";
-import { Canvas, useLoader } from "@react-three/fiber";
+import { Canvas, useLoader, useFrame } from "@react-three/fiber";
 import { Link } from "react-router-dom";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { useState } from "react";
+import { Vector3 } from "three";
 import items from "./items.json";
 
 export default function LandingPage() {
-  const hey = useGLTF.preload(items[0].src, true);
-  console.log(hey);
+  useGLTF.preload(items[0].src, true);
   const gltf = useLoader(GLTFLoader, "./studio.glb");
-
   const { progress, loaded, total } = useProgress();
+  const [isZoomedOut, setIsZoomedOut] = useState(true);
+
+  const CameraController = () => {
+    useFrame(({ camera }, delta) => {
+      const targetPosition = isZoomedOut
+        ? new Vector3(3, 3, 3)
+        : new Vector3(1, 1, 2);
+      const targetLookAt = isZoomedOut
+        ? new Vector3(0, 0, 0)
+        : new Vector3(0, 1, 0);
+
+      camera.position.lerp(targetPosition, delta * 3);
+      camera.lookAt(targetLookAt);
+    });
+    return null;
+  };
 
   return (
     <>
-      <Canvas camera={{ position: [4, 3, 3] }}>
+      <Canvas camera={{ position: [10, 10, 10] }}>
+        <CameraController />
         <ambientLight />
         <directionalLight position={[10, 10, 10]} />
-        <OrbitControls
-          autoRotate
-          autoRotateSpeed={2}
-          enablePan={false}
-          enableRotate={false}
-          enableZoom={false}
-        />
+        <OrbitControls enableZoom={false} />
         <primitive position={[0, 1, 0]} object={gltf.scene} />
       </Canvas>
+      <button
+        className={`toggle ${isZoomedOut ? "" : "in"}`}
+        onClick={() => setIsZoomedOut(!isZoomedOut)}
+      >
+        ↓
+      </button>
       {total === loaded ? (
         <Link to="/works">
-          <p className="loading-text">Link to items</p>
+          <p className="loading-text">Enter studio</p>
         </Link>
       ) : (
         <p className="loading-text">Loading studio: {Math.floor(progress)}%</p>
