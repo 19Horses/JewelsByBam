defe4d2aacb37032a0045f9e2468b53a659354af
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2025-03-12 11:24:12 +0000

    Fallback component for no webgl

--- src/landingPage.jsx
+++ src/landingPage.jsx
@@ -1,12 +1,14 @@
 import { OrbitControls, Text, useGLTF, useProgress } from "@react-three/drei";
-import { Canvas, useLoader, useFrame } from "@react-three/fiber";
-import { Link } from "react-router-dom";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
 import { useState } from "react";
-import { Vector3 } from "three";
-import items from "./items.json";
 import { isMobile } from "react-device-detect";
+import { ErrorBoundary } from "react-error-boundary";
+import { Link } from "react-router-dom";
+import { Vector3 } from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Fallback } from "./components/Fallback";
 import pixels from "./fonts/PerfectoCalligraphy.ttf";
+import items from "./items.json";
 
 export default function LandingPage() {
   useGLTF.preload(items[0].src, true);
@@ -41,29 +43,31 @@ export default function LandingPage() {
 
   return (
     <>
-      <Canvas camera={{ position: [10, 10, 10] }}>
-        <CameraController />
-        <ambientLight />
-        <directionalLight position={[10, 10, 10]} />
-        <OrbitControls
-          enableZoom={false}
-          minPolarAngle={0}
-          maxPolarAngle={Math.PI / 2}
-          minAzimuthAngle={-Math.PI / 4}
-          maxAzimuthAngle={Math.PI / 4}
-          enablePan={doneTransitioning}
-          enableRotate={doneTransitioning}
-        />
-        <Text
-          color="salmon"
-          font={pixels}
-          fontSize={0.15}
-          position={[-0.7, 1, -1.2]}
-        >
-          {"Bambi"}
-        </Text>
-        <primitive position={[0, 1, 0]} object={gltf.scene} />
-      </Canvas>
+      <ErrorBoundary fallback={<Fallback />}>
+        <Canvas camera={{ position: [10, 10, 10] }}>
+          <CameraController />
+          <ambientLight />
+          <directionalLight position={[10, 10, 10]} />
+          <OrbitControls
+            enableZoom={false}
+            minPolarAngle={0}
+            maxPolarAngle={Math.PI / 2}
+            minAzimuthAngle={-Math.PI / 4}
+            maxAzimuthAngle={Math.PI / 4}
+            enablePan={doneTransitioning}
+            enableRotate={doneTransitioning}
+          />
+          <Text
+            color="salmon"
+            font={pixels}
+            fontSize={0.15}
+            position={[-0.7, 1, -1.2]}
+          >
+            {"Bambi"}
+          </Text>
+          <primitive position={[0, 1, 0]} object={gltf.scene} />
+        </Canvas>
+      </ErrorBoundary>
       <button
         className={`toggle ${isMobile ? "mobile" : ""} ${
           isZoomedOut ? "" : "in"
