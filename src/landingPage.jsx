814872955fc4f176ece7944a61a3b19261e49b9f
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2025-03-12 11:53:52 +0000

    Landing page styles

--- src/landingPage.jsx
+++ src/landingPage.jsx
@@ -1,29 +1,37 @@
-import { OrbitControls, useGLTF } from "@react-three/drei";
+import { OrbitControls, useGLTF, useProgress } from "@react-three/drei";
 import { Canvas, useLoader } from "@react-three/fiber";
 import { Link } from "react-router-dom";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import items from "./items.json";
 
 export default function LandingPage() {
-  useGLTF.preload(items[0].src, true);
+  const hey = useGLTF.preload(items[0].src, true);
+  console.log(hey);
   const gltf = useLoader(GLTFLoader, "./studio.glb");
+
+  const { progress, loaded, total } = useProgress();
+
   return (
     <>
-      <Link to="/works">
-        <p className="entranceText text-red-800 z-10">LINK TO ITEMS</p>
-      </Link>
-      <Canvas camera={{ position: [0, 0, 1] }}>
+      <Canvas camera={{ position: [4, 3, 3] }}>
         <ambientLight />
         <directionalLight position={[10, 10, 10]} />
         <OrbitControls
-          minPolarAngle={0}
-          maxPolarAngle={Math.PI / 2}
-          minAzimuthAngle={-Math.PI / 4}
-          maxAzimuthAngle={Math.PI / 4}
+          autoRotate
+          autoRotateSpeed={2}
+          enablePan={false}
+          enableRotate={false}
           enableZoom={false}
         />
-        <primitive object={gltf.scene} />
+        <primitive position={[0, 1, 0]} object={gltf.scene} />
       </Canvas>
+      {total === loaded ? (
+        <Link to="/works">
+          <p className="loading-text">Link to items</p>
+        </Link>
+      ) : (
+        <p className="loading-text">Loading studio: {Math.floor(progress)}%</p>
+      )}
     </>
   );
 }
