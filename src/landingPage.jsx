954ae09d0d0e9d45673f9d06475c1182e7681327
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2025-03-12 11:24:12 +0000

    Fallback component for no webgl

--- src/landingPage.jsx
+++ src/landingPage.jsx
@@ -3,6 +3,8 @@ import { Canvas, useLoader } from "@react-three/fiber";
 import { Link } from "react-router-dom";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import items from "./items.json";
+import { ErrorBoundary } from "react-error-boundary";
+import { Fallback } from "./components/Fallback";
 
 export default function LandingPage() {
   useGLTF.preload(items[0].src, true);
@@ -12,18 +14,20 @@ export default function LandingPage() {
       <Link to="/works">
         <p className="entranceText text-red-800 z-10">LINK TO ITEMS</p>
       </Link>
-      <Canvas camera={{ position: [0, 0, 1] }}>
-        <ambientLight />
-        <directionalLight position={[10, 10, 10]} />
-        <OrbitControls
-          minPolarAngle={0}
-          maxPolarAngle={Math.PI / 2}
-          minAzimuthAngle={-Math.PI / 4}
-          maxAzimuthAngle={Math.PI / 4}
-          enableZoom={false}
-        />
-        <primitive object={gltf.scene} />
-      </Canvas>
+      <ErrorBoundary fallback={<Fallback />}>
+        <Canvas camera={{ position: [0, 0, 1] }}>
+          <ambientLight />
+          <directionalLight position={[10, 10, 10]} />
+          <OrbitControls
+            minPolarAngle={0}
+            maxPolarAngle={Math.PI / 2}
+            minAzimuthAngle={-Math.PI / 4}
+            maxAzimuthAngle={Math.PI / 4}
+            enableZoom={false}
+          />
+          <primitive object={gltf.scene} />
+        </Canvas>
+      </ErrorBoundary>
     </>
   );
 }
